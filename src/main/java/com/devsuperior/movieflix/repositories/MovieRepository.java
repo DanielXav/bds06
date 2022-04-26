06034e8848be8104b22712c0c40601cd68b776cb
Daniel Xavier <dxavier194@gmail.com>
2022-04-26 17:20:48 -0300

    GET /movies/id

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -6,12 +6,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
 
-@Repository	
-public interface MovieRepository extends JpaRepository<Movie, Long>{
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-	@Query("SELECT obj FROM Movie obj WHERE obj.genre = ':genreId'")
-	Page<Movie> findByName(Long genreId, Pageable pageable);
-	// ORDER BY obj.title ASC
+	@Query("SELECT obj FROM Movie obj ORDER BY obj.title ASC ")
+	Page<Movie> findByGenre(Pageable pageable);
+	// ORDER BY//obj.title ASC WHERE obj.genre = ':genreId'
+
+	// GENREID
+
+//		@Query("SELECT obj FROM Movie obj WHERE :genreId IS NULL OR obj.genre.id = :genreId")
+//		Page<Movie> findByGenre(Long genreId, Pageable pageable);
+//		// ORDER BY obj.title ASC WHERE obj.genre = ':genreId'
+
+	/*
+	 * @Query("SELECT obj FROM Movie obj WHERE :genre IS NULL OR obj.genre = :genre"
+	 * ) Page<Movie> findByGenre(Genre genre, Pageable pageable); // ORDER BY
+	 * obj.title ASC WHERE obj.genre = ':genreId'
+	 */
+
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,27 +1,37 @@
 package com.devsuperior.movieflix.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.MovieByIdDTO;
 import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class MovieService {
 	
 	@Autowired
 	private MovieRepository repository;
+	
+	@Autowired
+	private GenreRepository genreRepository;
 
 	@Transactional(readOnly = true)
 	public Page<MovieDTO> findAll(Long genreId, Pageable pageable) {
-		Page<Movie> genres = repository.findByName(genreId, pageable);
+//		Genre genre = (genreId == 0) ? null : genreRepository.getOne(genreId);
+		Page<Movie> genres = repository.findByGenre(pageable);
 		return genres.map(x -> new MovieDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieByIdDTO findById(Long id) {
+		Movie movie = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new MovieByIdDTO(movie);
 	}
 }
